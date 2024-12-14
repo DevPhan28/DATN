a886926f162bb7f8c16837cf42719fb1dbd982a6
Minmin48 <minhtdph36789@fpt.edu.vn>
2024-12-15 02:44:51 +0700

    fix: ui order user

--- fe/src/routes/_layout/orderuser/index.tsx
+++ fe/src/routes/_layout/orderuser/index.tsx
@@ -455,9 +455,6 @@ function UserOrder() {
                           Hủy đơn hàng
                         </button>
                       )}
-                      <button className="mr-2 rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600">
-                        áda
-                      </button>
                       {order.status === 'received' && (
                         <>
                           <button
