4e886a5bee843dd863bcd1a6ed7c3373db3c8729
DevPhan28 <taiptoh3317@fpt.edu.vn>
2024-12-17 22:53:44 +0700

    fix

--- fe/src/routes/dashboard/_layout/order/index.tsx
+++ fe/src/routes/dashboard/_layout/order/index.tsx
@@ -16,11 +16,8 @@ export const Route = createFileRoute('/dashboard/_layout/order/')({
 
 function OrderList() {
    const location = useLocation();
-      const status =location.state?.status 
-        ? location.state.status
-        : [];
-        console.log("log tâm trạng ", status);
-        const select = location.state?.selectedGroup || 'delivery';
+      const status =location.state?.status || 'all-delivery'
+      const select = location.state?.selectedGroup || 'delivery';
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedTab, setSelectedTab] = useState( status || 'all-delivery');
   const navigate = useNavigate();
